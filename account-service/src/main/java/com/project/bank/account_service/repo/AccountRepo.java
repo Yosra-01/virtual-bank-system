a7b15f5bce3396ebd9238bff84d50dd27592e50a
Yosra-01 <Y.Hassan0389@student.aast.edu>
2025-08-22 01:09:48 +0300

    feat: create Account repo

---
+++ account-service/src/main/java/com/project/bank/account_service/repo/AccountRepo.java
@@ -0,0 +1,13 @@
+package com.project.bank.account_service.repo;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.project.bank.account_service.model.Account;
+
+public interface AccountRepo extends JpaRepository<Account, UUID> {
+
+    List<Account> findByUserId(String userId);
+}
