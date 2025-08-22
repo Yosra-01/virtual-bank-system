42d49017fc7d5a7323e1151ef222a26e2584ba20
Yosra-01 <Y.Hassan0389@student.aast.edu>
2025-08-23 02:11:53 +0300

    add swaggers

--- account-service/src/main/java/com/project/bank/account_service/controller/AccountController.java
+++ account-service/src/main/java/com/project/bank/account_service/controller/AccountController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.project.bank.account_service.service.AccountService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 
 
 
@@ -25,6 +28,10 @@ public class AccountController {
     }
 
     //CREATE
+    @Operation(summary = "Create a new employee")
+    @ApiResponse(responseCode ="201", description = "Account created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid input data")
+
     @PostMapping("/accounts")
 
     public String createAccount(@RequestBody String entity) {
@@ -37,6 +44,11 @@ public class AccountController {
 
 
     //READ
+
+    @Operation(summary = "Get Account by ID")
+    @ApiResponse(responseCode = "200", description = "Account found")
+    @ApiResponse(responseCode = "404", description = "Account not found")
+
     @GetMapping("accounts/{accountId}")
 
     public String getAccount(String param) {
@@ -44,7 +56,10 @@ public class AccountController {
     }
 
 
-    
+    @Operation(summary = "Get all user Accounts")
+    @ApiResponse(responseCode = "200", description = "Account(s) found")
+    @ApiResponse(responseCode = "404", description = "No Account found")
+   // @ApiResponse(responseCode = "400", description = "User does not exist") wla user microservice issue???
     @GetMapping("/users/{userId}/accounts")
 
     public String getUserAccounts(String param) {
@@ -55,6 +70,11 @@ public class AccountController {
 
 
     //UPDATE
+    @Operation(summary = "Update employee by ID")
+    @ApiResponse(responseCode = "200", description = "Account updated successfully")
+    @ApiResponse(responseCode = "404", description = "Account not found")
+    @ApiResponse(responseCode = "409", description = "Insufficient funds to make the transfer") //409 conflict between request and resource availability
+
     @PutMapping("accounts/transfer")
 
     public String transfer(@PathVariable String id, @RequestBody String entity) {
