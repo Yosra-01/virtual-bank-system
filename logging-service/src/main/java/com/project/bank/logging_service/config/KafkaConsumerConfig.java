b8d5da8fad76354c3131e530522599066122ba34
Yosra-01 <Y.Hassan0389@student.aast.edu>
2025-08-26 23:13:19 +0300

    feat: consumer microservice setup

---
+++ logging-service/src/main/java/com/project/bank/logging_service/config/KafkaConsumerConfig.java
@@ -0,0 +1,8 @@
+package com.project.bank.logging_service.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+}
---
+++ logging-service/src/main/java/com/project/bank/logging_service/dto/LogMessage.java
@@ -0,0 +1,5 @@
+package com.project.bank.logging_service.dto;
+
+public class LogMessage {
+
+}
